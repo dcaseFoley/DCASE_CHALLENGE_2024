4839b3aa7a1262d27a1b51d58c2ff6d946e721e1
SAGNIK GHOSH <38028452+sagnik-charlie@users.noreply.github.com>
2024-04-14 14:52:08 +0530

    Update run.sh

--- code_foley_2023/run.sh
+++ code_foley_2023/run.sh
@@ -2,5 +2,6 @@ wget 'https://storage.googleapis.com/foley_dcase_baseline/g_00935000' -P checkpo
 wget 'https://storage.googleapis.com/foley_dcase_baseline/hifigan_config.json' -P checkpoint/hifi-gan
 wget 'https://storage.googleapis.com/foley_dcase_baseline/bottom_1501.pt' -P checkpoint/pixelsnail
 wget 'https://storage.googleapis.com/foley_dcase_baseline/vqvae_800.pt' -P checkpoint/vqvae
+pip install torch==1.13.1 librosa==0.10.0 python-lmdb tqdm
 
 python inference.py --vqvae_checkpoint checkpoint/vqvae/vqvae_800.pt --pixelsnail_checkpoint checkpoint/pixelsnail/bottom_1501.pt --number_of_synthesized_sound_per_class 10
